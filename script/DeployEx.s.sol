c0338bd74e8e3b552adb6678abd237b7d1ec1332
marc-antoine ourradour <ourradourm@gmail.com>
2024-08-25 17:40:13 +0200

    add ex03

--- script/DeployEx.s.sol
+++ script/DeployEx.s.sol
@@ -5,6 +5,7 @@ import "../lib/forge-std/src/Script.sol";
 import "../src/ERC20TD.sol";
 import "../src/Ex01.sol";
 import "../src/Ex02.sol";
+import "../src/Ex03.sol";
 
 contract DeployEx is Script {
     function run() external {
@@ -17,10 +18,12 @@ contract DeployEx is Script {
 
         Ex01 ex01 = new Ex01(erc20td);
         Ex02 ex02 = new Ex02(erc20td);
+        Ex03 ex03 = new Ex03(erc20td);
 
         address[] memory teachers = new address[](2);
         teachers[0] = address(ex01);
         teachers[1] = address(ex02);
+        teachers[2] = address(ex03);
         erc20td.setTeachers(teachers);
 
         vm.stopBroadcast();
@@ -30,7 +33,7 @@ contract DeployEx is Script {
         console.log("ERC20TD deployed at:", address(erc20td));
         console.log("Ex01 deployed at:", address(ex01));
         console.log("Ex02 deployed at:", address(ex02));
+        console.log("Ex03 deployed at:", address(ex03));
         console.log("-----------------");
-        console.log("Ex01 and Ex02 set as teachers for ERC20TD");
     }
 }
--- src/Ex03.sol
+++ src/Ex03.sol
@@ -3,10 +3,12 @@ pragma solidity ^0.8.19;
 
 import "./ExerciseTemplate.sol";
 
-contract Ex01 is ExerciseTemplate {
+contract Ex03 is ExerciseTemplate {
     constructor(ERC20TD _TDERC20) ExerciseTemplate(_TDERC20) {}
 
-    function ping() public {
+    function claimPoints(uint256 _studentUint) public {
+        require(_studentUint == 180618, "Value is incorrect");
+
         // Validating exercise
         creditStudent(2, msg.sender);
         validateExercise(msg.sender);
